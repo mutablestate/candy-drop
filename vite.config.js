fb1588a0bb6c9778459b6da3c24e76bd3eacbab0
mutablestate <benwebdev@gmail.com>
2022-01-19 04:47:39 +0700

    build: use pnpm overrides for borsh

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import nodePolyfills from "rollup-plugin-polyfill-node";
-import borsh from "borsh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), nodePolyfills()],
   define: {
-    borsh,
     "process.env.NODE_DEBUG": JSON.stringify(""),
   },
   optimizeDeps: {
